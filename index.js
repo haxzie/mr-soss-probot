a7f4fd8f25728eb7b76c2806cfa078c075754831
Musthaq Ahamad <musthu.gm@gmail.com>
2018-11-16 21:52:22 +0530

    test PR comment

--- index.js
+++ index.js
@@ -4,9 +4,19 @@ module.exports = app => {
 
   app.on('issues.opened', async context => {
     context.log("Issue opened");
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    console.log("Hey its the issue number : "+context.payload.issue.number);
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
+    console.log(issueComment);
     return context.github.issues.createComment(issueComment)
-  })
+  });
+
+  app.on('pull_request.opened', async context => {
+    context.log("PR opened");
+    console.log("Hey its the PR number : "+context.pull_request().number);
+    const prComment = context.issue({ body: 'Thanks for opening this PR!' });
+    console.log(prComment);
+    //return context.github.issues.createComment(issueComment)
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/

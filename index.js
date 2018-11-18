01cfb2c9dd199a9f3666b69ad24d4d8a812ab276
Musthaq Ahamad <musthu.gm@gmail.com>
2018-11-18 21:32:53 +0530

    Refactored Directory

--- index.js
+++ index.js
@@ -12,10 +12,15 @@ module.exports = app => {
 
   app.on('pull_request.opened', async context => {
     context.log("PR opened");
-    console.log("Hey its the PR number : "+context.pull_request().number);
-    const prComment = context.issue({ body: 'Thanks for opening this PR!' });
-    console.log(prComment);
-    //return context.github.issues.createComment(issueComment)
+    let prNumber = context.issue().number;
+
+    let prMessage = "Hey there :wave: Thanks for the PR! \n";
+    prMessage+= "Here\'s the link for the [deploy preview](https://deploy-preview-"+prNumber+"--sosc.netlify.com/) of the changes. \n";
+    prMessage+= "Deploy preview will be available in a while if the changes didn\'t break anything. ";
+    prMessage+= "Fingers crossed :crossed_fingers:";
+
+    const prComment = context.issue({ body: prMessage});
+    return context.github.issues.createComment(prComment);
   });
 
   // For more information on building apps:
